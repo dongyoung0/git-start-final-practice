e07dc8d94608e2a7ed2891faaf451fa9cf1ce00a
dongyoung0 <wmkimdy@gmail.com>
2021-08-23 01:50:49 +0900

    feat: add function about leaf year

--- final-practice.py
+++ final-practice.py
@@ -2,6 +2,14 @@
 
 ##1 사용자의 입력(1~3000 사이의 정수)년도 가 윤년인지 알려주는 function
 
+def leaf_year():
+    year = int(input("년도를 입력하세요: "))
+    if year > 3000 or year <1:
+        print("1~3000사이의 정수를 입력해주세요.")
+    elif year%400==0 or (year%4==0 and year%100!=0):
+        print("윤년입니다")
+    else: 
+        print("평년입니다")
 
 ##2 ethiopian multiplication
 
