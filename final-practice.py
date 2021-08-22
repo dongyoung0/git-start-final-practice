32dd027a5ec314b3396b609decc92736646d5ec8
dongyoung0 <wmkimdy@gmail.com>
2021-08-23 01:33:59 +0900

    feat: Final Practice
    Create final-practice.py

---
+++ final-practice.py
@@ -0,0 +1,19 @@
+#Final Practice
+
+##1 사용자의 입력(1~3000 사이의 정수)년도 가 윤년인지 알려주는 function
+
+
+##2 ethiopian multiplication
+
+
+##3 fibonacci sequence(recursion)
+
+
+##4 fibonacci sequence(with memoization)
+
+
+
+##5 fibonacci sequence(with binet's formula)
+
+
+
