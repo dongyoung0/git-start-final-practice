4dbfaa019669af29172cf513048bab2802848386
dongyoung0 <wmkimdy@gmail.com>
2021-08-23 03:08:20 +0900

    fix: Add Title of final-practice.py
    Add Title #Final Practice to py file

--- final-practice.py
+++ final-practice.py
@@ -1,26 +1,17 @@
 #Final Practice
-##1 사용자의 입력(1~3000 사이의 정수)년도 가 윤년인지 알려주는 function
 
-def leaf():
-    year = int(input("년도를 입력하세요: "))
-    if year > 3000 or year <1:
-        print("1~3000사이의 정수를 입력해주세요.")
-    elif year%400 == 0 or (year%4 == 0 and year%100 != 0):
-        print("윤년입니다")
-    else: 
-        print("평년입니다")
+##1 Leaf Year function
+
+##2 Ethiopian multiplication
+
+##3 Fibonacci sequence(recursion)
 
 
-##2 ethiopian multiplication
-
-##3 fibonacci sequence(recursion)
-
-
-##4 fibonacci sequence(with memoization)
+##4 Fibonacci sequence(with memoization)
 
 
 
-##5 fibonacci sequence(with binet's formula)
+##5 Fibonacci sequence(with binet's formula)
 
 
 
