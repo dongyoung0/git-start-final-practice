cc1d2e1cc324423cb831362679592bfeb4994a84
dongyoung0 <wmkimdy@gmail.com>
2021-08-23 02:20:40 +0900

    feat: add ethiopian multiplication function
    Add Ethiopian multiplication function with python

--- final-practice.py
+++ final-practice.py
@@ -6,10 +6,23 @@
 ##2 ethiopian multiplication
 
 
+def eth_mult():
+    a = int(input("첫번째 수를 입력해주세요: "))
+    b = int(input("두번째 수를 입력해주세요: "))
+    result = 0
+    while a>0:
+        if a%2 == 1:
+            result += b
+        a //= 2
+        b *= 2
+    return result
+
+
 ##3 fibonacci sequence(recursion)
 
 
 ##4 fibonacci sequence(with memoization)
+
 
 
 
